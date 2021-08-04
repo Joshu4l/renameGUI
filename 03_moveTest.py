65f02b6d42ccb6d8a8fb347f85ae96cc699c620b
Joshu4l <77698069+Joshu4l@users.noreply.github.com>
2021-08-04 21:58:24 +0200

    Delete 03_moveTest.py

--- 03_moveTest.py
+++
@@ -1,8 +0,0 @@
-targetFolder = r"C:\Users\joshua.albert\OneDrive - PUMA\Desktop\pyTestForRenaming\929411_01_01_KOR.jpg"
-
-
-destinationFolder = r"C:\Users\joshua.albert\OneDrive - PUMA\Desktop\pyTestForRenaming\new"
-
-
-
-shutil.copy(targetFolder, destinationFolder)
